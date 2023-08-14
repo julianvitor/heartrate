05c54701fa4a83f2311640180eb250b26d691d5b
Julian Carreiro <julian.vtr@gmail.com>
2023-08-14 18:48:47 -0300

    Atualizar o serial_input.py

--- serial_input.py
+++ serial_input.py
@@ -11,9 +11,9 @@ import random
 #    return tensao_atual
 
 
-# Função para simular a obtenção de dados de tensão do Arduino (substitua por sua função real)
+# Função para simular a obtenção de dados de tensão do Arduino (substitua pela função real quando em hardware) 
 def serial_input():
     # Suponhamos que você tem uma variável chamada "tensao_atual" que contém o valor de tensão
     # Substitua este trecho pela leitura real da porta serial do Arduino
-    tensao_atual = random.randint(1500, 5000)  # Supondo valores entre 0 e 5 volts
+    tensao_atual = random.randint(1500, 5000)  # Supondo valores entre 1.5 e 5 volts
     return tensao_atual
